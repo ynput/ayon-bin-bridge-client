0df05bf50ee40b2e0088caaa872cbe4422dade28
Lyon_Rosenblatt <48656065+lyon040502003@users.noreply.github.com>
2024-06-19 08:27:44 +0200

    added zip_foulder func to allow ziping a foulder

--- ayon_bin_distro/util/zip.py
+++ ayon_bin_distro/util/zip.py
@@ -1,4 +1,7 @@
+import re
+from typing import Optional, Union
 import zipfile
+import os
 
 
 def extract_zip_file(progress_item, zip_file_path: str, dest_dir: str) -> str:
@@ -14,3 +17,23 @@ def extract_zip_file(progress_item, zip_file_path: str, dest_dir: str) -> str:
 
     foulder_name = os.path.basename(zip_file_path).replace(".zip", "")
     return os.path.join(dest_dir, foulder_name)
+
+
+def zip_folder(folder_path: str, output_path: str) -> Union[str, bool]:
+
+    if not os.path.isdir(folder_path):
+        return False
+
+    if not output_path.endswith(".zip"):
+        return False
+
+    folder_path = os.path.abspath(folder_path)
+
+    with zipfile.ZipFile(output_path, "w", zipfile.ZIP_DEFLATED) as zipf:
+        for root, _, files in os.walk(folder_path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                arcname = os.path.relpath(file_path, folder_path)
+                zipf.write(file_path, arcname)
+
+    return output_path
